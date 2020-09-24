de0ca50c83a179f263e62ebfbee3d89e23648227
LHlh22 <731560818@qq.com>
2020-09-24 17:55:06 +0800

    Site updated: 2020-09-24 17:55:05

--- js/my.js
+++ js/my.js
@@ -2,3 +2,4 @@ var xiaokang = new xkTool();
 xiaokang.cheatTitle(['(＝^ω^＝)Miss you'],['Welcome(∩_∩)']);
 xiaokang.footFish();
 xiaokang.meting("5250313077","netease","playlist");
+xiaokang.codeFull();
